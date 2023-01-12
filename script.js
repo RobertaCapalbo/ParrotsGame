57add348d9004b6e3f4e8f306daaff05caf24733
Roberta Capalbo <robertacapalbo@hotmail.com>
2023-01-12 11:36:16 -0300

    Versão 50% Pronta

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-prompt("Com quantas cartas você gostaria de jogar? São permitidos apenas números pares de 2 a 14")
+let num = Number(prompt("Com quantas cartas você gostaria de jogar? São permitidos apenas números pares de 4 a 14"));
+if(num===2){
+    alert("Poxa, número de cartas inválido. Tente novamente com um número entre 4 e 14")
+}
+if(num===3){
+    alert("Poxa, número de cartas inválido. Tente novamente com um número entre 4 e 14")
+}
+if(num===5){
+    alert("Poxa, número de cartas inválido. Tente novamente com um número entre 4 e 14")
+}
+if(num===7){
+    alert("Poxa, número de cartas inválido. Tente novamente com um número entre 4 e 14")
+}
+if(num===9){
+    alert("Poxa, número de cartas inválido. Tente novamente com um número entre 4 e 14")
+}
+if(num===11){
+    alert("Poxa, número de cartas inválido. Tente novamente com um número entre 4 e 14")
+}
+if(num===13){
+    alert("Poxa, número de cartas inválido. Tente novamente com um número entre 4 e 14")
+}
+if(num>14){
+    alert("Poxa, número de cartas inválido. Tente novamente com um número entre 4 e 14")
+}
+
+
